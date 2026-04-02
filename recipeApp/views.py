553d2b2ba67a37fac1e1b0427e66a5f91a896b7e
mellie789 <melliezel789@gmail.com>
2026-04-02 11:47:59 -0500

    Worked on templates

--- recipeApp/views.py
+++ recipeApp/views.py
@@ -4,3 +4,17 @@ from django.contrib import messages
 
 def home(request):
 	return render(request, 'home.html',{})
+
+
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Account created successfully")
+			return redirect ("login")
+	else:
+		form = UserCreationForm()
+
+	return render(request, "register.html", {"form":form})
+
