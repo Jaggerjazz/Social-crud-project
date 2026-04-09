7e096c9effd75e44a04a0cdebf9c243fcf425b48
mellie789 <melliezel789@gmail.com>
2026-04-09 13:02:54 -0500

    errors

--- testProject/urls.py
+++ testProject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from recipeApp import views  # <--- THIS IS THE IMPORTANT LINE
+from recipeApp import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipeApp.urls')), # <--- This connects to the file you just showed me
+    path('', include('recipeApp.urls')), 
 ]
