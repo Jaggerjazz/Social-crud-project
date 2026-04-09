c4580a5a7f4d27dd7ae3292e1cdf2ceb47a0db24
Jaggerjazz <jasmine.cervantes01@utrgv.edu>
2026-04-09 12:52:57 -0500

    Fixing errors

--- recipeApp/urls.py
+++ recipeApp/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('register/', views.register, name = 'register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
+    # Home Page
+    path('', views.home, name='home'),
+    
+    # Login & Logout (Built-in Django)
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='home.html'), name='logout'),
+    
+    # Registration (The missing link!)
+    path('register/', views.register, name='register'),
 ]
