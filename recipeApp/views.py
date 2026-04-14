1603313d9c5c94632d0fae120f26fe361d00b674
mellie789 <melliezel789@gmail.com>
2026-04-14 13:43:20 -0500

    Started on the profile and what the user can do once logged in

--- recipeApp/views.py
+++ recipeApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def home(request):
 	return render(request, 'home.html',{})
@@ -18,5 +19,17 @@ def register(request):
 
 	return render(request, "register.html", {"form":form})
 
+@login_required
+def profile(request):
+    return render(request, 'profile.html', {'user': request.user})
 
+@login_required
+def my_recipes(request):
+    # Your logic for user's recipes
+    return render(request, 'my_recipes.html')
+
+@login_required
+def favorites(request):
+    # Your logic for favorites
+    return render(request, 'favorites.html')
 
