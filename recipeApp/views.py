60552c9e4312081e327b07abc4535a93e71fb6a0
mellie789 <melliezel789@gmail.com>
2026-04-28 13:16:15 -0500

    edit

--- recipeApp/views.py
+++ recipeApp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Recipe, Profile
+from .models import Recipe, Profile 
 from .forms import RecipeForm
 
 def home(request):
